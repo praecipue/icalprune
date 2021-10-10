9de0c2634ee0592b3912ea41c0dc837c5709ebd3
praecipue <43908213+praecipue@users.noreply.github.com>
2021-10-10 22:16:41 +0200

    Option not to push when identical output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
-#!/bin/sh -l
+#!/bin/bash
+PUSH=0
+python3 ${PROCESS_ICAL_PATH} ${INPUT_DAYS} &
+mkdir -p public && echo ${RUN_NUMBER} > public/index.html
+[ -n "${COMPARE_URL}" ] && { wget -q "${COMPARE_URL}" -O prev.tsv || PUSH=1; }
+fg
+[ ${PUSH} = 0 ] && diff -q -s prev.tsv public/pruned.tsv && exit 0 || true
 cd public
 # based on https://github.com/BryanSchuetz/jekyll-deploy-gh-pages/blob/master/entrypoint.sh
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
